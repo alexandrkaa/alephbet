a337cd93c1dc267bbbb63d2fc207c50666de5c78
Aleksandr Komarov <komar81@gmail.com>
2022-09-09 19:48:36 +0300

    initial commit

---
+++ alephbet.js
@@ -0,0 +1,72 @@
+const elLetter = document.getElementById(`letter`);
+const form = document.getElementById(`answer-form`);
+const elAnswerList = document.getElementById(`answer-list`);
+const ANSWERS_NUM = 5;
+
+const getLettersSet = () => {
+  const result = {};
+
+  let letterRnd = randomInt(0, dictionary.length - 1);
+  let letter = dictionary[letterRnd];
+  result.currentLetter = letter;
+  let answerOptions = [];
+  answerOptions.push(letter);
+  let i = 1;
+  while (i < ANSWERS_NUM) {
+    letterRnd = randomInt(0, dictionary.length - 1);
+    letter = dictionary[letterRnd];
+
+    if (!answerOptions.find((it) => it?.l === letter.l)) {
+      answerOptions.push(letter);
+      i++;
+    }
+  }
+  result.answerOptions = shuffle(answerOptions);
+
+  return result;
+};
+
+const getAnswerInput = (letter) => {
+  const li = document.createElement(`li`);
+  const lbl = document.createElement(`label`);
+  const spn = document.createElement(`span`);
+  const inp = document.createElement(`input`);
+  spn.innerText = letter.t;
+  inp.setAttribute(`type`, `radio`);
+  inp.setAttribute(`name`, `answerLetter`);
+  inp.value = letter.l;
+  lbl.appendChild(inp);
+  lbl.appendChild(spn);
+  li.appendChild(lbl);
+  return li;
+};
+
+const renderQuiz = (lettersSet) => {
+  elLetter.innerHTML = lettersSet.currentLetter.l;
+  elAnswerList.innerHTML = ``;
+  for (let i = 0; i < lettersSet.answerOptions.length; i++) {
+    elAnswerList.appendChild(getAnswerInput(lettersSet.answerOptions[i]));
+  }
+};
+
+let lettersSet = getLettersSet();
+
+form.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+  const selected = form.querySelector(`input:checked`);
+  if (selected.value === lettersSet.currentLetter.l) {
+    elLetter.innerHTML = `ניצחון`;
+    setTimeout(() => {
+      lettersSet = getLettersSet();
+      renderQuiz(lettersSet);
+    }, 1500);
+  } else {
+    elAnswerList.classList.add(`shake`);
+  }
+});
+
+elAnswerList.addEventListener(`animationend`, (evt) => {
+  evt.target.classList.remove(`shake`);
+});
+
+renderQuiz(lettersSet);
---
+++ utils.js
@@ -0,0 +1,15 @@
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
