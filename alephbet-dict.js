cce3720b15003091b78ac78b6b8164b0a658d66f
Aleksandr Komarov <komar81@gmail.com>
2024-01-01 18:46:11 +0100

    handwrite version

---
+++ alephbet-dict.js
@@ -0,0 +1,24 @@
+const cutLetter = (arr, idx) => {
+  return [...arr.slice(0, idx), ...arr.slice(idx + 1, arr.length)]
+}
+const elLetter = document.getElementById(`letter`);
+let dictCopy = [...dictionary];
+let letterRnd = randomInt(0, dictCopy.length - 1);
+let letter = dictCopy[letterRnd];
+dictCopy = cutLetter(dictCopy, letterRnd);
+elLetter.innerHTML = letter.t
+
+document.addEventListener('keypress', (evt) => {
+  evt.preventDefault();
+  elLetter.innerHTML = letter.l;
+  setTimeout(() => {
+    if (dictCopy.length === 0) {
+      console.log(`restart`);
+      dictCopy = [...dictionary];
+    }
+    letterRnd = randomInt(0, dictCopy.length - 1);
+    letter = dictCopy[letterRnd];
+    dictCopy = cutLetter(dictCopy, letterRnd);
+    elLetter.innerHTML = letter.t
+  }, 1000);
+})
