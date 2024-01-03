2689d7ef9ffd21a4a212e855c0fcd2a087f0c4c1
Aleksandr Komarov <komar81@gmail.com>
2024-01-03 20:38:45 +0100

    up

--- alephbet-dict.js
+++ alephbet-dict.js
@@ -23,17 +23,3 @@ document.addEventListener('keypress', (evt) => {
   }, 1000);
 })
 
-document.addEventListener('ontouchstart', (evt) => {
-  evt.preventDefault();
-  elLetter.innerHTML = letter.l;
-  setTimeout(() => {
-    if (dictCopy.length === 0) {
-      console.log(`restart`);
-      dictCopy = [...dictionary];
-    }
-    letterRnd = randomInt(0, dictCopy.length - 1);
-    letter = dictCopy[letterRnd];
-    dictCopy = cutLetter(dictCopy, letterRnd);
-    elLetter.innerHTML = letter.t
-  }, 1000);
-})
