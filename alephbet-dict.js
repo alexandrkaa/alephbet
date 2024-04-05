e8dc6e16e898db98f635e6b1a60d7a028fb3ff67
Aleksandr Komarov <komar81@gmail.com>
2024-04-05 08:53:16 +0300

    new words, including 4 part

--- alephbet-dict.js
+++ alephbet-dict.js
@@ -5,12 +5,17 @@ const letterView = {
   visible: `t`,
   hidden: `l`
 }
+let type = `letters`;
 const elLetter = document.getElementById(`letter`);
 let dictCopy = [...dictionary];
 let letterRnd = randomInt(0, dictCopy.length - 1);
 let letter = dictCopy[letterRnd];
 dictCopy = cutLetter(dictCopy, letterRnd);
 elLetter.innerHTML = letter[letterView.hidden];
+const totalEl = document.querySelector(`#total > span`);
+const tillEndEl = document.querySelector(`#till-end > span`);
+totalEl.innerText = dictionary.length;
+tillEndEl.innerText = dictionary.length;
 
 const fontForm = document.querySelector("#font");
 fontForm.addEventListener('change', (evt) => {
@@ -22,6 +27,7 @@ fontForm.addEventListener('change', (evt) => {
 
 const typeForm = document.querySelector(`#type`);
 typeForm.addEventListener('change', (evt) => {
+  type = evt.target.value;
   if (evt.target.value === `letters`) {
     letterView.visible = `t`;
     letterView.hidden = `l`;
@@ -31,20 +37,49 @@ typeForm.addEventListener('change', (evt) => {
   }
 });
 
+const renderLetter = () => {
+  if (dictCopy.length === 0) {
+    console.log(`restart`);
+    dictCopy = [...dictionary];
+  }
+  tillEndEl.innerText = dictCopy.length;
+  letterRnd = randomInt(0, dictCopy.length - 1);
+  letter = dictCopy[letterRnd];
+  dictCopy = cutLetter(dictCopy, letterRnd);
+  elLetter.innerHTML = letter[letterView.visible]
+}
+
+let step = 1;
 const toNextLetter = (evt) => {
   evt.preventDefault();
-  elLetter.innerHTML = letter[letterView.hidden];
-  setTimeout(() => {
-    if (dictCopy.length === 0) {
-      console.log(`restart`);
-      dictCopy = [...dictionary];
-    }
-    letterRnd = randomInt(0, dictCopy.length - 1);
-    letter = dictCopy[letterRnd];
-    dictCopy = cutLetter(dictCopy, letterRnd);
-    elLetter.innerHTML = letter[letterView.visible]
-  }, 2000);
+  if (step === 1) {
+    elLetter.innerHTML = letter[letterView.hidden];
+    step = 2;
+  } else {
+    renderLetter();
+    step = 1;
+  }
+
 }
+
+// const toNextLetter = (evt) => {
+//   evt.preventDefault();
+//   console.log(type);
+//   if (type !== `letters`) {
+//     elLetter.innerHTML = letter[letterView.hidden];
+//     setTimeout(() => {
+//       renderLetter();
+//     }, 2000);
+//   } else {
+//     console.log(step);
+//     if (step === 2) {
+//       step = 1;
+//       renderLetter();
+//     }
+//     elLetter.innerHTML = letter[letterView.hidden];
+//     step = 2;
+//   }
+// }
 
 document.addEventListener(`keypress`, toNextLetter);
 document.addEventListener(`touchstart`, toNextLetter);
